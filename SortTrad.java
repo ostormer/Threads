67ba4577f8fbac835e3d7c12bd5e7554720dd6e2
ostormer <oskar.stormer@gmail.com>
2016-04-13 15:55:05 +0200

    bruker getMonitor

--- SortTrad.java
+++ SortTrad.java
@@ -11,21 +11,21 @@ class SortTrad extends Thread{
         String min = arr1[0];
         for(int i = 0; i < arr1.length-1; i++){
             if(arr1[i].compareTo(arr1[i+1]) == 1){
-                int temp = arr1[i];
-                arr1[i] = arr[i+1];
+                String temp = arr1[i];
+                arr1[i] = arr1[i+1];
                 arr1[i+1] = temp;
                 i = -1;
             }
         }
-        monitor.putInn(arr1);
+        Sort.getMonitor().putInn(arr1);
 
         boolean harJobb = true;
 
         while(harJobb){
-            ToArrayer to = monitor.hentTo();
+            ToArrayer to = Sort.getMonitor().hentTo();
             String[] sendDenne = flett(to.arr1, to.arr2);
 
-            monitor.putInn(sendDenne);
+            Sort.getMonitor().putInn(sendDenne);
         }
     }
 
