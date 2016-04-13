c361da98c63bdf931592139e8fc1a117f655c569
ostormer <oskar.stormer@gmail.com>
2016-04-13 15:29:46 +0200

    Added files via upload
    Mangler plan for å avslutte tråden, og fungerende kommunikasjon med monitor

--- SortTrad.java
+++ SortTrad.java
@@ -13,12 +13,20 @@ class SortTrad extends Thread{
             if(arr1[i].compareTo(arr1[i+1]) == 1){
                 int temp = arr1[i];
                 arr1[i] = arr[i+1];
-                arri[i+1] = temp;
+                arr1[i+1] = temp;
                 i = -1;
             }
         }
-        monitor.taImot();
-        monitor.hentTo();
+        monitor.putInn(arr1);
+
+        boolean harJobb = true;
+
+        while(harJobb){
+            ToArrayer to = monitor.hentTo();
+            String[] sendDenne = flett(to.arr1, to.arr2);
+
+            monitor.putInn(sendDenne);
+        }
     }
 
     public String[] flett(String[] arr1, String[] arr2){
