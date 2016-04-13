eecf58c407b0ac09072543c2a2bd683399a5027d
heneij <o-lyn@online.no>
2016-04-13 13:00:49 +0200

    feilmelding på argumenter og på filen

--- Sort.java
+++ Sort.java
@@ -1,8 +1,31 @@
-import java.io.*
-import java.util.*
+import java.io.*;
+import java.util.*;
 
 class Sort{
-  public static void main(String[] args) {
-    String antOrd = args[0];
+  public static void main(String[] args) throws FileNotFoundException{
+
+    File innFil = new File(args[1]);
+    Scanner sc = new Scanner(innFil);
+    try{
+      int antTraader = Integer.parseInt(args[0]);
+    }
+    catch(NumberFormatException e){
+      System.out.println("Det foerste argumentet er ikke et tall");
+      System.exit(0);
+    }
+    int antOrd = Integer.parseInt(sc.nextLine());
+
+    String[] ordliste = new String[antOrd];
+    try{
+      for(int i = 0; i < antOrd; i++){
+        ordliste[i] = sc.nextLine();
+      }
+    }
+    catch(NoSuchElementException e){
+      System.out.println("Det er for faa ord i Filen");
+    }
+    if(sc.hasNextLine()){
+      System.out.println("Det er for mange ord i filen");
+    }
   }
 }
