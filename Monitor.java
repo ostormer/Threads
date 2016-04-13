0930918388c1fb210f3fb6c07c8544a666524e51
ostormer <oskar.stormer@gmail.com>
2016-04-13 16:12:00 +0200

    Indent

--- Monitor.java
+++ Monitor.java
@@ -1,27 +1,27 @@
 class Monitor {
-  private String[] arr1;
-  private String[] arr2;
+    private String[] arr1;
+    private String[] arr2;
 
-  synchronized void putInn(String[] arr){
-    while(arr1 != null && arr2 != null){
-      try{
-        wait();
-      }
-      catch(InterruptedException e){
-        System.exit(0);
-      }
-    }
-    if(arr1 == null){
-      arr1 = arr;
-    }
-    else{
-      arr2 = arr;
+    synchronized void putInn(String[] arr){
+        while(arr1 != null && arr2 != null){
+            try{
+                wait();
+            }
+            catch(InterruptedException e){
+                System.exit(0);
+            }
+        }
+        if(arr1 == null){
+            arr1 = arr;
+        }
+        else{
+            arr2 = arr;
+        }
+
     }
 
-  }
-
-  synchronized ToArrayer hentTo(){
-    /*while(arr1 == null || arr2 == null){
+    synchronized ToArrayer hentTo(){
+        /*while(arr1 == null || arr2 == null){
 
     }*/
     String[] temp1 = arr1;
@@ -31,5 +31,5 @@ class Monitor {
     notify();
     return new ToArrayer(temp1, temp2);
 
-  }
+}
 }
